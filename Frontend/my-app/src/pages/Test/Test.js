5c6f2df116f1c6a8831e2de447bc86a449ef3e68
Haris Aqeel <115533883+HarisSp4rk@users.noreply.github.com>
2022-10-22 02:39:15 +0500

    directory structure

---
+++ Frontend/my-app/src/pages/Test/Test.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Page from 'material-ui-shell/lib/containers/Page'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useIntl } from 'react-intl'
+import { Grid,Paper, Avatar, TextField, Button, Typography } from '@material-ui/core'
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+const Test = () => {
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px 0px 0px 800px",flex:-1}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+    return(
+        <Grid
+        >
+            
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                     <Avatar style={avatarStyle}></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
+
+};
+export default Test
