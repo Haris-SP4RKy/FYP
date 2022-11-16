3eb8412eef4d00531991afeb0a3eeb22c9eea7d0
Haris Aqeel <115533883+HarisSp4rk@users.noreply.github.com>
2022-11-16 19:06:18 +0500

    Kafka egress

--- Backend/Webserver/db/pg/migrations/20221109111230_schema.js
+++ Backend/Webserver/db/pg/migrations/20221109111230_schema.js
@@ -4,25 +4,25 @@
  */
 exports.up = async (knex) => {
     await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-	await knex.schema
-		.createTable('data', function (table) {
-			table.increments('id');
-			table.text('deviceId').notNullable();
-			// table.text('CO2');
-			// table.text('CO');
-			// table.text('airQuality');
-			// table.text('long');
-			// table.text('lat');
-			table.binary('data').notNullable();
-			table.index(['deviceId']);
-			// table.index(['long', 'lat']);
-			table.timestamps(true, true).notNull();
-		});
+	// await knex.schema
+	// 	.createTable('data', function (table) {
+	// 		table.increments('id');
+	// 		table.text('deviceId').notNullable();
+	// 		// table.text('CO2');
+	// 		// table.text('CO');
+	// 		// table.text('airQuality');
+	// 		// table.text('long');
+	// 		// table.text('lat');
+	// 		table.binary('data').notNullable();
+	// 		table.index(['deviceId']);
+	// 		// table.index(['long', 'lat']);
+	// 		table.timestamps(true, true).notNull();
+	// 	});
         await knex.schema
 		.createTable('users', (table) => {
-			table.increments('id').primary();
+			table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
 			table.text('name').notNullable();
-			table.text('email').notNullable();
+			table.text('email').notNullable().unique();
 			table.text('password').notNullable();
 			table.text('avatar').defaultTo('https://i.imgur.com/Xq2bZCY.png');
 			table.text('bio').defaultTo('I am a new user');
@@ -30,6 +30,52 @@ exports.up = async (knex) => {
 			table.index(['email']);
 			table.timestamps(true, true).notNull();
 		});
+
+
+	await knex.schema
+	.createTable('devices', function (table) {
+		table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+		table.text('device_serial').notNullable();
+		table.text('sensor_type');
+		table.index(['deviceId']);
+		table.timestamps(true, true).notNull();
+	});
+
+	await knex.schema
+	.createTable('sensor_data', function (table) {
+		table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+		table.text('device_id').notNullable().references('id').inTable('devices').onDelete('CASCADE');
+		table.text('sensor_type');
+		table.binary('data').notNullable();
+		table.index(['device_id']);
+		// table.index(['long', 'lat']);
+		table.timestamps(true, true).notNull();
+	});
+	await knex.schema
+	.createTable('devices_group', function (table) {
+		table.increments('id').primary();
+		table.text('name').notNullable();
+		table.text('latitude').notNullable();
+		table.text('longitude').notNullable();
+		table.timestamps(true, true).notNull();
+	});
+	await knex.schema
+	.createTable('groups', function (table) {
+		table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+		table.text('device_id').notNullable().references('id').inTable('devices').onDelete('CASCADE');
+		table.text('group_id').notNullable().references('id').inTable('devices_group').onDelete('CASCADE');
+		table.index(['device_id']);
+		table.timestamps(true, true).notNull();
+	});
+
+	await knex.schema
+	.createTable('user_access', function (table) {
+		table.uuid('id').unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+		table.text('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');;
+		table.text('group_id').notNullable().references('id').inTable('devices_group').onDelete('CASCADE');
+		table.timestamps(true, true).notNull();
+	});
+	
 };
 
 /**
