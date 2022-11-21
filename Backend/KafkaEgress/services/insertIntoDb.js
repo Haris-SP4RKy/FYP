daa9be42f38c32a92903de45edb4b8dda347fb43
Haris Aqeel <115533883+HarisSp4rk@users.noreply.github.com>
2022-11-21 20:26:20 +0500

    Some changes

--- Backend/KafkaEgress/services/insertIntoDb.js
+++ Backend/KafkaEgress/services/insertIntoDb.js
@@ -1,4 +1,15 @@
-
+const db  = require('../db/pg/db');
+/**
+ * //
+ * message={
+ * device_id:uuid
+ * sensor_type:string
+ * data:obj
+ * 
+ * }
+ * @param {*} message 
+ */
 module.exports.Kafkaegress=async (message)=>{
-
+    const recordId = await db('sensor_data').insert(message).returning('id')
+const groupName=await db.select('u.name','u.longitude','u.latitude').from('devices_group')
 }
